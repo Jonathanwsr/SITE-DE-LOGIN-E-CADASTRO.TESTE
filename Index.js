2f4a0aad224de86703799c945f09db34922b600e
Jonathan <126990110+Jonathanwsr@users.noreply.github.com>
2024-08-17 15:59:19 -0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,15 @@
-Index.js
+if (localStorage.getItem("token") == null) {
+    alert("Você precisa estar logado para acessar essa página");
+    window.location.href = "./assets/signin.html";
+  }
+  
+  const userLogado = JSON.parse(localStorage.getItem("userLogado"));
+  
+  const logado = document.querySelector("#logado");
+  logado.innerHTML = `Olá ${userLogado.nome}`;
+  
+  function sair() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userLogado");
+    window.location.href = "./assets/signin.html";
+  }
